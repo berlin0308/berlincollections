9142545bf0d1d85e9d87dc6b5fc35fe87795cda9
berlin0308 <90204339+berlin0308@users.noreply.github.com>
2021-09-27 19:51:09 +0800

    Add files via upload

---
+++ other_projects/tkinter-checkbox.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from _cffi_backend import callback
+ 
+def callBackFunc():
+    print("Oh. I'm clicked")
+    
+app = tk.Tk() 
+app.geometry('150x100')
+
+chkValue = tk.BooleanVar() 
+chkValue.set(True)
+ 
+chkExample = tk.Checkbutton(app, text='Check Box', var=chkValue, command=callBackFunc) 
+chkExample.grid(column=0, row=0)
+
+app.mainloop()
